830fa55fb5941a92052bcf31a007f7e3348b9f70
tkdalsss <lsm991229@naver.com>
2022-12-07 19:57:43 +0900

    update watchDetailInfo.js

--- src/components/WatchDetailInfo.js
+++ src/components/WatchDetailInfo.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import '../css/WatchDetailInfo.css';
+import axios from 'axios';
 
 function WatchDetailInfo(){
 
