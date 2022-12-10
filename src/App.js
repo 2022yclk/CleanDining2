eef0d40d63df621870397c344be5df591a1ae08e
keymaker9 <90887310+keymaker9@users.noreply.github.com>
2022-12-10 18:22:17 +0900

    완성본(아마도) 라우팅된 App.js
    App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,12 @@ import CreateNewParty from './components/CreateNewParty.js'
 import Form from './pages/Form.js'
 import AdminPage from './pages/AdminPage';
 import ShowReview from './components/ShowReview.js'; // add show Review new
-import MyPage from './components/MyPage';
+import SearchParty from './pages/SearchParty.js';
+import PartyGen from './pages/PartyGen.js';
+import Info from './pages/Info.js';
+import Review from './pages/Review.js';
+import JoinParty from './pages/JoinParty.js';
+import Mypage from './pages/Mypage.js';
 
 function App() {
   return (
@@ -38,7 +43,12 @@ function App() {
        <Route path="/createNewParty/:id/:key" element={<CreateNewParty/>}></Route>
        <Route path="/form" element={<Form/>}></Route>
        <Route path="/admin" element={<AdminPage/>} />
-       <Route path="/mypage" element={<MyPage />}/>
+       <Route path="/SearchParty/:id/:key/:addr/:addr2/:grade/:phone/:cat" element={<SearchParty />} />
+       <Route path="/PartyGen/:id/:key" element={<PartyGen />} />
+       <Route path="/Info/:id/:key/:addr/:addr2/:grade/:phone/:cat" element={<Info />} />
+       <Route path="/Review/:id/:key" element={<Review />} />
+       <Route path="/JoinParty/:key/:postid" element={<JoinParty />}></Route>\
+       <Route path="/Mypage" element={<Mypage />}/>
       </Routes>
     </Router>
   )
